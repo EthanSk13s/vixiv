ddcbd3538f367ae1e1e9edc15b5c4389411a1c0f
EthanSk13s <ethanmiranda11@gmail.com>
2022-10-29 03:11:58 -0700

    add example index

---
+++ application/ui/src/components/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <main>
+            <section class="flex-container row">                  
+            </section>
+        </main>
+        <div id="counter" class="flex-container column">
+
+        </div>
+    </div>
+</template>
