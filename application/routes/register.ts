5600ef344f76a5f10b1b1da1302839821bde92e8
EthanSk13s <ethanmiranda11@gmail.com>
2022-10-30 12:57:21 -0700

    add register logic

--- application/routes/register.ts
+++ application/routes/register.ts
@@ -1,11 +1,29 @@
 import { Request, Response, NextFunction, Router } from "express";
+import * as bcrypt from "bcrypt";
 
+import { db } from "./connection";
+
+const SALT_ROUNDS: number = 15;
 var router = Router();
 
+// https://stackoverflow.com/a/56478076 for now, figure out something better
+function genID() {
+    return Date.now() + ((Math.random()*100000).toFixed())
+}
+
+function registerUser(username: string, email: string, pass: string) {
+    let hashedPass: string = bcrypt.hashSync(pass, SALT_ROUNDS);
+
+    db.query(
+        `
+        INSERT INTO users(id, username, password, email)
+            VALUES(?, ?, ?, ?)
+        `, [genID(), username, hashedPass, email]
+    );
+}
+
 router.post('/', function (req: Request, res: Response, next: NextFunction) {
-    console.log(req.body.user);
-    console.log(req.body.email);
-    console.log(req.body.password);
+    registerUser(req.body.user, req.body.email, req.body.password);
 
     res.redirect('/login');
 })
