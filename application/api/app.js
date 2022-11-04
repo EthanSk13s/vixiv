2c42e159ce198c2c85e0bbbb9da4a70b0c31b1b5
EthanSk13s <ethanmiranda11@gmail.com>
2022-11-03 22:37:05 -0700

    remove unusde files from merge

--- application/api/app.js
+++ application/api/app.js
@@ -11,7 +11,6 @@ const usersRouter = require("./routes/users");
 const loginRouter = require("./routes/login");
 const registerRouter = require("./routes/register");
 const postImageRouter = require("./routes/post_image");
-const examplePostRouter = require("./routes/example_post");
 
 const app = express();
 const staticFileMiddleWare = express.static(path.join(__dirname, '../ui/dist'));
