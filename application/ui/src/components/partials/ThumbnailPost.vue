888c50da4d272dbf38468c21e5aeed3c7a313238
EthanSk13s <ethanmiranda11@gmail.com>
2022-11-03 22:28:07 -0700

    add thumnail post component

---
+++ application/ui/src/components/partials/ThumbnailPost.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export default {
+        props: {
+            author: String,
+            authorPfp: String,
+            title: String,
+            image: String
+        }
+    }
+</script>
+
+<template>
+    <div class="flex-container post-flex">
+        <div class="post-container">
+            <div class="thumbnail-container">
+                <img class="thumbnail-container" :src=image alt="">
+            </div>
+            {{ title }}
+            <div class="author-container">
+                <div class="author-pfp">
+                    <img :src=authorPfp alt="" class="author-pfp">
+                </div>
+                <div>
+                    {{ author }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
