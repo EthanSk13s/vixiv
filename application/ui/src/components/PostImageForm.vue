cc52bfc7a716775c171488c1744df6cd3b0b4b1d
EthanSk13s <ethanmiranda11@gmail.com>
2022-11-04 01:42:47 -0700

    add image preview

--- application/ui/src/components/PostImageForm.vue
+++ application/ui/src/components/PostImageForm.vue
@@ -8,6 +8,50 @@ export default {
         Checkbox,
         SubmitButton,
         TextInput
+    },
+    methods: {
+        displayImage(files: FileList) {
+            for (let i = 0; i < files.length; i++) {
+                const file: File = files[i];
+                if (file.type.startsWith('image/')) {
+                    const currentImg: HTMLImageElement = document.getElementById('preview') as HTMLImageElement;
+                    if (currentImg) {
+                        const reader = new FileReader();
+                        reader.onload = (e) => { currentImg.src = e.target?.result as string; };
+                        reader.readAsDataURL(file);
+                    } else {
+                        const img: HTMLImageElement = document.createElement('img');
+                        const thumbnail = document.getElementById('file-upload');
+                        img.setAttribute('id', 'preview');
+                        img.classList.add('preview-img');
+
+                        thumbnail?.after(img);
+
+                        const reader = new FileReader();
+                        reader.onload = (e) => { img.src = e.target?.result as string; };
+                        reader.readAsDataURL(file);
+                    }
+                }
+            }
+
+        },
+        addFileListener() {
+            let fileUpload = document.getElementById('file-upload');
+
+            fileUpload?.addEventListener('input', this.inputFile);
+        },
+        inputFile(e: Event) {
+            e.stopPropagation();
+            e.preventDefault();
+
+            const dt = e.currentTarget as HTMLInputElement;
+            const files = dt.files;
+
+            this.displayImage(files!);
+        }
+    },
+    mounted() {
+        this.addFileListener()
     }
 }
 </script>
@@ -17,15 +61,16 @@ export default {
         <h2>Post an Image</h2>
         <form>
             <fieldset class="main-field">
-                <TextInput label-name="imageTitle" place-holder="Title"/>
-                <TextInput label-name="imageDesc" place-holder="Description"/>
-                <div class="column-md">
-                    <label class="input-label" for="image">Image(s)</label>
-                    <input class="input-file" type="file" name="images" id="images" multiple required>
+                <div class="flex-container column">
+                    <label for="file-upload" class="post-button">
+                        Select image
+                    </label>
+                    <input id="file-upload" type="file" accept="image/*" required />
                 </div>
-                <br>
-                <Checkbox name="aup" label-desc="Before posting, please accept the Acceptable Use Policy"/>
-                <SubmitButton submit-text="Post Image"/>
+                <TextInput label-name="imageTitle" place-holder="Title" />
+                <TextInput label-name="imageDesc" place-holder="Description" />
+                <Checkbox name="aup" label-desc="Before posting, please accept the Acceptable Use Policy" />
+                <SubmitButton submit-text="Post Image" />
             </fieldset>
         </form>
     </section>
