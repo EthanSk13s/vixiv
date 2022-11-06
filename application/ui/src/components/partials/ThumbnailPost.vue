b4b1aa6074e9074ef43185232ff5df0144d6c339
EthanSk13s <ethanmiranda11@gmail.com>
2022-11-05 20:15:11 -0700

    wrap in a tags

--- application/ui/src/components/partials/ThumbnailPost.vue
+++ application/ui/src/components/partials/ThumbnailPost.vue
@@ -1,29 +1,32 @@
 <script lang="ts">
-    export default {
-        props: {
-            author: String,
-            authorPfp: String,
-            title: String,
-            image: String
-        }
+export default {
+    props: {
+        author: String,
+        authorPfp: String,
+        title: String,
+        image: String,
+        postPath: String
     }
+}
 </script>
 
 <template>
-    <div class="flex-container post-flex">
-        <div class="post-container">
-            <div class="thumbnail-container">
-                <img class="thumbnail-container" :src=image alt="">
-            </div>
-            {{ title }}
-            <div class="author-container">
-                <div class="author-pfp">
-                    <img :src=authorPfp alt="" class="author-pfp">
+    <a class="no-deco" :href="postPath">
+        <div class="flex-container post-flex">
+            <div class="post-container">
+                <div class="thumbnail-container">
+                    <img class="thumbnail-container" :src=image alt="">
                 </div>
-                <div>
-                    {{ author }}
+                {{ title }}
+                <div class="author-container">
+                    <div class="author-pfp">
+                        <img :src=authorPfp alt="" class="author-pfp">
+                    </div>
+                    <div>
+                        {{ author }}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </a>
 </template>
