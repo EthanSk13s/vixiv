334f82ee9d5b0e3ad145403c74dbaf66ef42548f
EthanSk13s <ethanmiranda11@gmail.com>
2022-11-06 20:00:16 -0800

    convert requests to new params

--- application/ui/src/components/Index.vue
+++ application/ui/src/components/Index.vue
@@ -1,12 +1,12 @@
 <script lang="ts">
 import InfoContainer from '@/components/partials/InfoContainer.vue';
-import { stringifyQuery } from 'vue-router';
 import ThumbnailPost from './partials/ThumbnailPost.vue';
 
 export default {
     data() {
         return {
-            latestPosts: [] as { title: any; authorName: any; path: string; postPath: string }[]
+            latestPosts: [] as { title: any; authorName: any; path: string; postPath: string }[],
+            userPosts: [] as { title: any; authorName: any; path: string; postPath: string }[]
         }
     },
     components: {
@@ -15,10 +15,10 @@ export default {
     },
     methods: {
         getLatestPosts() {
-            this.$http.get('/api/image/posts/')
+            this.$http.get('/api/image/posts/', {params: {limit: 15}})
                 .then((response) => {
                     let posts: { title: any; authorName: any; path: string; postPath: string }[] = []
-                    response.data.data.forEach((element: any) => {
+                    response.data.forEach((element: any) => {
                         let post = {
                             title: element.title,
                             authorName: element.authorName,
@@ -30,10 +30,31 @@ export default {
                     });
                     this.latestPosts = posts;
                 })
+        },
+        getUserPosts() {
+            this.$http.get('/api/image/posts/', {params: {limit: 15, user: localStorage.getItem('id')}})
+                .then((response) => {
+                    let posts: { title: any; authorName: any; path: string; postPath: string }[] = []
+                    response.data.forEach((element: any) => {
+                        let post = {
+                            title: element.title,
+                            authorName: element.authorName,
+                            path: `/public/storage/images/${element.postId}.png`,
+                            postPath: `/post/${element.postId}`
+                        }
+
+                        posts.push(post);
+                    });
+                    this.userPosts = posts;
+                })
         }
     },
     mounted() {
-        this.getLatestPosts()
+        this.getLatestPosts();
+
+        if (localStorage.getItem('id')) {
+            this.getUserPosts();
+        }
     }
 }
 </script>
@@ -47,25 +68,23 @@ export default {
                     <h2>Latest Posts</h2>
                 </div>
                 <div class="flex-container row post-flex">
-                    <ThumbnailPost v-for="post in latestPosts" 
+                    <ThumbnailPost v-if="latestPosts" v-for="post in latestPosts" 
                         :author="post.authorName"
                         authorPfp="https://theater.miriondb.com/card_bg/029umi0314_0.png"
                         :title="post.title" 
                         :image="post.path"
                         :postPath="post.postPath"/>
                 </div>
-                <div>
+                <div v-if="userPosts.length != 0">
                     <h2>Your Posts</h2>
                 </div>
                 <div class="flex-container row post-flex">
-                    <ThumbnailPost author="AliceBob20" authorPfp="https://theater.miriondb.com/icons/017kth0343_0.png" 
-                        title="Cuteoha" image="https://theater.miriondb.com/card_bg/017kth0054_0.png"/>
-                    <ThumbnailPost author="AliceBob20" authorPfp="https://theater.miriondb.com/icons/017kth0343_0.png" 
-                        title="Cuteoha 2" image="https://theater.miriondb.com/card_bg/017kth0124_0.png"/>
-                    <ThumbnailPost author="AliceBob20" authorPfp="https://theater.miriondb.com/icons/017kth0343_0.png" 
-                        title="Cuteoha 3" image="https://theater.miriondb.com/card_bg/017kth0154_0.png"/>
-                    <ThumbnailPost author="AliceBob20" authorPfp="https://theater.miriondb.com/icons/017kth0343_0.png" 
-                        title="Cuteoha 4" image="https://theater.miriondb.com/card_bg/017kth0174_1.png"/>
+                    <ThumbnailPost v-if="userPosts.length != 0" v-for="post in userPosts" 
+                        :author="post.authorName"
+                        authorPfp="https://theater.miriondb.com/card_bg/029umi0314_0.png"
+                        :title="post.title" 
+                        :image="post.path"
+                        :postPath="post.postPath"/>
                 </div>
             </section>
         </main>
