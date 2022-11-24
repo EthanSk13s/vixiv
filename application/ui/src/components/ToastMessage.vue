9ba70d10a483d8105b22f46c4dadcfc9cb305db0
EthanSk13s <ethanmiranda11@gmail.com>
2022-11-24 17:49:19 +0100

    add toast messages

---
+++ application/ui/src/components/ToastMessage.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+
+export default {
+    props: {
+        type: String,
+        message: String,
+    },
+    updated() {
+        console.log('test')
+        let toasty = this.$refs.toasty as HTMLElement;
+
+        toasty.classList.remove('hidden');
+        setTimeout(() => {
+            toasty.classList.add('hidden');
+        }, 5000)
+    }
+}
+</script>
+
+<template>
+    <div class="flex-container">
+        <div ref="toasty" class="toasty hidden">
+            {{ message }}
+        </div>
+    </div>
+
+</template>
+
+<style>
+.toasty {
+    border-radius: 0.5em;
+    padding: .5em;
+    background-color: blue;
+    position: absolute;
+    display: flex;
+    z-index: 98;
+    overflow-x: auto;
+    justify-content: center;
+    margin-top: 1.9em;
+    top: 0;
+    transition: top 0.25s ease-in-out;
+}
+
+.toasty.hidden {
+    top: -100px;
+}
+</style>
