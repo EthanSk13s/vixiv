765b68a94860538661836aa4533e6aff1da08770
EthanSk13s <ethanmiranda11@gmail.com>
2022-11-26 18:58:53 -0800

    add register toasty

--- application/api/routes/register.ts
+++ application/api/routes/register.ts
@@ -3,26 +3,34 @@ import * as bcrypt from "bcrypt";
 
 import { db } from "./connection";
 import { genID } from "../helpers/id-gen";
+import { RowDataPacket } from "mysql2";
 
 const SALT_ROUNDS: number = 15;
 var router = Router();
 
-async function registerUser(username: string, email: string, pass: string) {
+async function registerUser(res: Response, username: string, email: string, pass: string) {
     let hashedPass: string = await bcrypt.hash(pass, SALT_ROUNDS);
     const conn = db.promise();
 
-    await conn.query(
-        `
-        INSERT INTO users(id, username, password, email)
-            VALUES(?, ?, ?, ?)
-        `, [genID(), username, hashedPass, email]
-    );
+    const [rowsLike, fields] = await conn.query(`SELECT * FROM users WHERE username=?`, username);
+    const check = rowsLike as RowDataPacket[];
+
+    if (check.length < 1) {
+        await conn.query(
+            `
+            INSERT INTO users(id, username, password, email)
+                VALUES(?, ?, ?, ?)
+            `, [genID(), username, hashedPass, email]
+        );
+
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(409)
+    }
 }
 
 router.post('/', async function (req: Request, res: Response, next: NextFunction) {
-    await registerUser(req.body.user, req.body.email, req.body.password);
-
-    res.redirect('/login');
+    await registerUser(res, req.body.user, req.body.email, req.body.password);
 })
 
 module.exports = router;
--- application/ui/src/components/RegisterForm.vue
+++ application/ui/src/components/RegisterForm.vue
@@ -3,11 +3,54 @@ import SubmitButton from './partials/SubmitButton.vue';
 import TextInput from './partials/TextInput.vue';
 import Checkbox from './partials/Checkbox.vue';
 
+import { toastStore } from '@/stores/toast';
+
 export default {
+    setup() {
+        const toast = toastStore();
+
+        return {
+            toast
+        }
+    },
+    data() {
+        return {
+            username: "",
+            password: ""
+        }
+    },
     components: {
         SubmitButton,
         TextInput,
         Checkbox
+    },
+    methods: {
+        tryRegister(event: Event) {
+            event.preventDefault();
+            let form = this.$refs.registerForm as HTMLFormElement;
+            let data = new FormData(form);
+
+            // Create JSON data
+            let json = {
+                user: data.get('user'),
+                email: data.get('email'),
+                password: data.get('password')
+            }
+
+            this.$http.post("/api/register", json)
+                .then((response) => {
+                    if (response.status == 200) {
+                        this.toast.$patch({ type: 'success' });
+                        this.toast.$patch({ message: 'Successfully Registered' });
+
+                        this.$router.push({ 'name': 'login' });
+                    }
+                })
+                .catch((error) => {
+                    this.toast.$patch({ type: 'error' });
+                    this.toast.$patch({ message: 'Failed to Register. There may be a duplicate' });
+                })
+        }
     }
 }
 </script>
@@ -15,9 +58,9 @@ export default {
 <template>
     <section class="flex-container column">
         <h2>Registration</h2>
-        <form id="main-form" action="/api/register" method="POST">
+        <form ref="registerForm" id="main-form" @submit="tryRegister" method="POST">
             <fieldset class="main-field">
-                <TextInput label-name="user" place-holder="Username"/>
+                <TextInput label-name="user" place-holder="Username" />
                 <div class="column-md">
                     <label class="input-label" for="email">Email</label>
                     <input class="input-text" type="email" name="email" placeholder="E-mail" required>
@@ -30,10 +73,11 @@ export default {
                     <label class="input-label" for="confirmPass">Confirm Password</label>
                     <input id="confirmPass" class="input-text" type="password" placeholder="Confirm Password" required>
                 </div>
-                <Checkbox name="over13" label-desc="Are you over the age of 13"/>
+                <Checkbox name="over13" label-desc="Are you over the age of 13" />
                 <div class="column-md">
                     <input class="checkmark" type="checkbox" name="tosAndPrivacy" required>
-                    <label for="tosAndPrivacy">Do you accept our <a href="">Terms of Service</a> and <a href="">PrivacyPolicy</a></label>
+                    <label for="tosAndPrivacy">Do you accept our <a href="">Terms of Service</a> and <a
+                            href="">PrivacyPolicy</a></label>
                 </div>
                 <div class="column-md">
                     <input class="post-button" type="submit" value="Register">
