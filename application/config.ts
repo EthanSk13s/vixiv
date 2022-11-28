bb6a70ecc1f94de0e02337213aad80fab05bbb84
EthanSk13s <ethanmiranda11@gmail.com>
2022-11-28 10:57:57 -0800

    add global config

---
+++ application/config.ts
@@ -0,0 +1,9 @@
+export const CONFIG = {
+    DB_USER: "vixiv",
+    DB_PASS: "vixiv",
+    THUMBNAIL_PATH: '/public/storage/thumbnails',
+    PFP_PATH: '/public/storage/profiles',
+    POST_PATH: '/post',
+    LATEST_POST_LIMIT: 14,
+    USER_POST_LIMIT: 14,
+}
